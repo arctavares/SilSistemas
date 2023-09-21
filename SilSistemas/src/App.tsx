54f2140f3b9715cc286cb26177c9778d4db64672
arctavares <cosilk@hotmail.com>
2023-09-21 16:25:04 -0300

    cria estrutura inicial da pagina

---
+++ SilSistemas/src/App.tsx
@@ -0,0 +1,28 @@
+import './App.css'
+
+function App() {
+
+  return (
+    <div className='body'>
+      <div className='header'>
+        Sil Sistemas
+      </div>
+      <div className='bottomContainer'>
+        
+      </div>
+      <div className='cardsContainer'>
+        <div className='covidCasesContainer card'>
+          COVID
+        </div>
+        <div className='newsContainer card'>
+          NEWS
+        </div>
+        <div className='reviewContainer card'>
+          REVIEW
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default App
